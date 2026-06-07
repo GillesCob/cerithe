2a460fd3d6882e13966e95e0ea57c7431711263a
Gilles <contact@gillescobigo.com>
2026-06-07 21:41:13 +0200

    feat: add Prisma client singleton with PrismaPg adapter

---
+++ backend/src/services/auth.service.ts
@@ -0,0 +1,8 @@
+export const register = async (email: string, password: string) => {
+    // Vérifier si l'email existe en BDD
+    const emailExist = 
+    // Si oui lancer une erreur
+    // Si non hasher le mdp
+    // créer le user en BDD
+    // return user sans pwd
+}
